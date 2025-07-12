f5214ab557774ab33eb4faba2bd2fb6fa74f1cad
Sanskruti <edakesanskruti9771@gmail.com>
2025-07-12 13:53:34 +0530

    added new navbar

---
+++ src/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar(){
+    return(
+        <div>
+            <h3>Name:Sanskruti</h3>
+            <h3>Class:Ty-Co</h3>
+            <h3>Email:edakesanskruti@gmail.com</h3>
+        </div>
+    )
+}
+
+export default Navbar;
